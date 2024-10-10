f10f0e208136d55ee1a3c47a4b246d975ced3b17
Keldon Lee <wlee-96@outlook.com>
2024-10-10 12:37:44 +0000

    feat:

--- element.sh
+++ element.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Define the PSQL variable to connect to the PostgreSQL database
-PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+PSQL="psql --username=postgres --dbname=periodic_table -t --no-align -c"
 
 # Check if an argument is provided
 if [[ -z $1 ]]; then
@@ -24,3 +24,7 @@ ELEMENT=$($PSQL "$QUERY")
 if [[ -z $ELEMENT ]]; then
   echo "I could not find that element in the database."
 else
+  # Parse the result and display the element's details
+  IFS="|" read -r ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT"
+  echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+fi
